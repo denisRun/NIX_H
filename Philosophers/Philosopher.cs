7539d315d302ad4a8c869ee31dfd2162a659b6ee
denisRun <polozov.denis77@gmail.com>
2021-05-28 12:37:42 +0300

    Philosophers

---
+++ Philosophers/Philosopher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Philosophers
+{
+    class Philosopher
+    {
+        public int id;
+
+        public Philosopher(int id)
+        {
+            this.id = id;
+        }
+
+        static Mutex mutex = new Mutex(false, "Forks");
+
+        public void FindForks(List<Fork> forks)
+        {
+            lock (mutex)
+            {
+                int ind1 = id;
+                int ind2;
+                if (id != forks.Count-1)
+                    ind2 = id + 1;
+                else
+                    ind2 = 0;
+
+                if (forks[ind1].isFree)
+                {
+                    forks[ind1].isFree = false;
+                    Console.WriteLine($"Philosopher {id+1} took fork in LEFT hand");
+                    Thread.Sleep(100);
+                    if (forks[ind2].isFree)
+                    {
+                        forks[ind2].isFree = false;
+                        Console.WriteLine($"Philosopher {id+1} took fork in RIGHT hand");
+                        Thread.Sleep(100);
+                        Console.WriteLine($"Philosopher {id+1} is eating");
+                        forks[ind1].isFree = true;
+                        Console.WriteLine($"Philosopher {id+1} put LEFT fork");
+                        Thread.Sleep(100);
+                        forks[ind2].isFree = true;
+                        Console.WriteLine($"Philosopher {id+1} put RIGHT fork");
+                        Console.WriteLine($"Philosopher {id+1} is thinking");
+                    }
+                    else
+                    {
+                        forks[ind1].isFree = false;
+                        Console.WriteLine($"Philosopher {id+1} put LEFT fork");
+                    }
+                }
+            }
+        }
+
+        public void Start(object forks)
+        {
+            for(int i = 0; i < 5; i++) { 
+                Console.WriteLine($"Philosopher {id+1} is looking for forks");
+                FindForks((List<Fork>)forks);
+                Random rnd = new Random();
+                Thread.Sleep(rnd.Next(1, 2000));                
+            }
+        }
+    }
+}
---
+++ Philosophers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Philosophers
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Fork> forks = new List<Fork>();
+            List<Philosopher> philosophers = new List<Philosopher>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Fork fork = new Fork();
+                Philosopher philosopher = new Philosopher(i);
+                forks.Add(fork);
+                philosophers.Add(philosopher);
+            }
+
+            Thread t1 = new Thread(philosophers[0].Start);
+            Thread t2 = new Thread(philosophers[1].Start);
+            Thread t3 = new Thread(philosophers[2].Start);
+            Thread t4 = new Thread(philosophers[3].Start);
+            Thread t5 = new Thread(philosophers[4].Start);
+
+            t1.Start(forks);
+            t2.Start(forks);
+            t3.Start(forks);
+            t4.Start(forks);
+            t5.Start(forks);
+
+        }
+
+    }
+}
